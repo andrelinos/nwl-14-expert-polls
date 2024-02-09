b18a31d9ce27bd81590cc6ef61bac6118684e9c0
Andrelino Silva <andrelinodev@gmail.com>
2024-02-09 17:44:04 -0300

    🚀 Install Docker, Postgres, redis, prisma

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,8 +2,12 @@ import fastify from 'fastify'
 
 const app = fastify()
 
+app.get('/', () => {
+  return 'Hello world!'
+})
+
 const PORT = 3333
 
 app.listen({ port: PORT }).then(() => {
-  console.log(`HTTP is running on port ${PORT} ::fire:`)
+  console.log(`🚀 HTTP is running on port ${PORT} 🔥`)
 })
